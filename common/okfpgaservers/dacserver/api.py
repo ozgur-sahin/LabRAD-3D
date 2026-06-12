487fbb60be2319cdad72c05327713da08715424c
ChiMasterChi <an123d@gmail.com>
2026-06-12 14:51:42 -0700

    first steps towards fixing a dacserver bug
    there is a minor dacserver bug where if you stop the server and try to
    restart it, it gives an error and crashes but then it starts up normally
    again if you try to start it again. It might be a usb handle issue
    where the server doesn't release some usb connections when the server
    stops.

--- common/okfpgaservers/dacserver/api.py
+++ common/okfpgaservers/dacserver/api.py
@@ -13,12 +13,14 @@ class api(object):
     
     def connectOKBoard(self):
         fp = ok.FrontPanel()
+        # print "Done Not High Error Code=", ok.FrontPanel.DoneNotHigh
         module_count = fp.GetDeviceCount()
         print "Found {} unused modules".format(module_count)
         for i in range(module_count):
             serial = fp.GetDeviceListSerial(i)
             tmp = ok.FrontPanel()
-            tmp.OpenBySerial(serial)
+            ret = tmp.OpenBySerial(serial)
+            # print "OpenBySerial returned: ", ret
             iden = tmp.GetDeviceID()
             if iden == self.okDeviceID:
                 self.xem = tmp
@@ -28,12 +30,23 @@ class api(object):
         return False
     
     def programOKBoard(self):
+        print "Is xem open:", self.xem.IsOpen()
+        self.xem.ResetFPGA()
+        print "resetted FPGA"
+        import time
+        time.sleep(0.2)
         prog = self.xem.ConfigureFPGA(self.okDeviceFile)
-        if prog: raise("Not able to program FPGA")
+        if prog: 
+            print "Not able to program FPGA, error code: ", prog
+            raise Exception("Not able to program FPGA, error code: " + str(prog))
+        print "ConfigureFPGA finished"
         pll = ok.PLL22150()
+        print "pll initialization successful."
         self.xem.GetEepromPLL22150Configuration(pll)
+        print "GetEeprom successful"
         pll.SetDiv1(pll.DivSrc_VCO,4)
         self.xem.SetPLL22150Configuration(pll)
+        print "Programmed OK Board"
         
     def programBoard(self, sequence):
         self.xem.WriteToBlockPipeIn(0x80, 2, sequence)
